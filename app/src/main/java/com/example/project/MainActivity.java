350a714c02929b280ff62ca3ac83f3d51ba20314
crystallee069 <crystallee069@gmail.com>
2022-05-13 13:26:06 +0800

    notification setting

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -4,7 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -23,6 +29,33 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ScheduleFragment()).commit();
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.overflow_setting:
+                if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.O){
+                    Intent noti = new Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+                    noti.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
+                    startActivity(noti);
+                }else{
+                    Intent notNoti = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                    notNoti.setData(Uri.parse("package:" + getPackageName()));
+                    startActivity(notNoti);
+                }
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+
     private NavigationBarView.OnItemSelectedListener navListener = new NavigationBarView.OnItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
