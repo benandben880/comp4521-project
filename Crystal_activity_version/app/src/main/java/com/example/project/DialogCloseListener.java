6d0db7173dfd2fb12bb27fc398bf7d68a2e2515e
sunsunv3v <83497757+sunsunv3v@users.noreply.github.com>
2022-05-17 16:18:22 +0800

    merge code

---
+++ Crystal_activity_version/app/src/main/java/com/example/project/DialogCloseListener.java
@@ -0,0 +1,8 @@
+package com.example.project;
+
+import android.content.DialogInterface;
+
+public interface DialogCloseListener {
+
+    public void handleDialogClose(DialogInterface dialog);
+}
---
+++ Crystal_activity_version/app/src/main/java/com/example/project/ScheduleActivity.java
@@ -0,0 +1,47 @@
+package com.example.project;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class ScheduleActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+        TextView demo = (TextView) findViewById(R.id.scheduletext);
+        demo.setText("the schedule activity");
+
+        BottomNavigationView Nav = findViewById(R.id.bottom_nav);
+        Nav.setOnItemSelectedListener(navListener);
+    }
+
+    private NavigationBarView.OnItemSelectedListener navListener = new NavigationBarView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch(item.getItemId()){
+                case R.id.nav_schedule:
+                    Intent intent = new Intent(ScheduleActivity.this, ScheduleActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.nav_memo:
+                    Intent intent1 = new Intent(ScheduleActivity.this, MemoActivity.class);
+                    startActivity(intent1);
+                    break;
+                case R.id.nav_todoList:
+                    Intent intent2 = new Intent(ScheduleActivity.this, ToDoActivity.class);
+                    startActivity(intent2);
+                    break;
+            }
+            return true;//selected item
+        }
+    };
+}
